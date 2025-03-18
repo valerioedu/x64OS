e2371b5cdd3de5a6f8af4e93765f7a7e35390abb
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-03-19 00:47:26 +0100

    Final touch with the keyboard for now

--- kernel/drivers/keyboard/keyboard.c
+++ kernel/drivers/keyboard/keyboard.c
@@ -1,5 +1,4 @@
 #include "keyboard.h"
-#include "toascii.h"
 #include "../../cpu/src/pic.h"
 #include "../../../lib/definitions.h"
 #include "../vga/vga.h"
@@ -8,6 +7,62 @@
 
 volatile uint8_t g_last_scancode = 0;
 volatile char    g_last_char     = 0;
+
+static const char scancode_to_ascii_table[128] = {
+    [0x01] = KEY_ESC,
+    [0x0E] = '\b',
+    [0x0F] = '\t',
+    [0x1C] = '\n',
+    [0x39] = ' ',
+
+    [0x1E] = 'a',
+    [0x30] = 'b',
+    [0x2E] = 'c',
+    [0x20] = 'd',
+    [0x12] = 'e',
+    [0x21] = 'f',
+    [0x22] = 'g',
+    [0x23] = 'h',
+    [0x17] = 'i',
+    [0x24] = 'j',
+    [0x25] = 'k',
+    [0x26] = 'l',
+    [0x32] = 'm',
+    [0x31] = 'n',
+    [0x18] = 'o',
+    [0x19] = 'p',
+    [0x10] = 'q',
+    [0x13] = 'r',
+    [0x1F] = 's',
+    [0x14] = 't',
+    [0x16] = 'u',
+    [0x2F] = 'v',
+    [0x11] = 'w',
+    [0x2D] = 'x',
+    [0x15] = 'y',
+    [0x2C] = 'z',
+
+    [0x02] = '1',
+    [0x03] = '2',
+    [0x04] = '3',
+    [0x05] = '4',
+    [0x06] = '5',
+    [0x07] = '6',
+    [0x08] = '7',
+    [0x09] = '8',
+    [0x0A] = '9',
+    [0x0B] = '0',
+
+    [0x1A] = '[',
+    [0x1B] = ']',
+    [0x2B] = '\\',
+    [0x27] = ';',
+    [0x28] = '\'',
+    [0x33] = ',',
+    [0x34] = '.',
+    [0x35] = '/',
+    [0x29] = '`'
+};
 
 void keyboard_init() {
     outb(0x60, 0xF4);
@@ -22,7 +77,7 @@ void isr_keyboard_handler() {
     uint16_t scancode = (uint16_t)inb(0x60);
     g_last_scancode = scancode;
     if (KEY_IS_PRESS(scancode)) {
-        g_last_char = KEY_SCANCODE(scancode);
+        g_last_char = scancode_to_ascii_table[scancode];
     }
     
     pic_send_eoi(1);
@@ -48,8 +103,7 @@ char* read() {
         while (keyboard_get_char() == 0) {
         }
         
-        int i = keyboard_get_scancode();
-        char c = scancode_to_ascii(i);
+        char c = keyboard_get_char();
         g_last_char = 0;
 
         if (c == KEY_ENTER || c == KEY_RETURN) {
