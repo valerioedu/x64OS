708cc09e1a57f9c3713f3b39d255c0833141bcfb
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-03-19 00:51:36 +0100

    just a test

--- kernel/threading/src/process.h
+++ kernel/threading/src/process.h
@@ -44,7 +44,19 @@ typedef struct __attribute__((packed)) CpuState {
 } CpuState;
 
 typedef struct FpuState {
-
+    uint8_t buffer[512];
+    uint16_t status;
+    uint16_t control;
+    uint16_t tag;
+    uint16_t opcode;
+    uint64_t ip_offset;
+    uint64_t ip_selector;
+    uint64_t data_offset;
+    uint64_t data_selector;
+    uint8_t st[8][10];
+    uint8_t mm[8][8];
+    uint8_t xmm[8][16];
+    uint8_t padding[96];
 } FpuState;
 
 typedef struct Process {        //basic, but should do the job for now
