ce1f4aa85d85c3f8af9e58022ddf31c92bebbce2
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-03-31 23:44:12 +0200

    syscalls

--- kernel/syscalls/sys_stat/stat.h
+++ kernel/syscalls/sys_stat/stat.h
@@ -2,6 +2,7 @@
 #define STAT_H
 
 #include "../../../lib/definitions.h"
+#include "../../fs/src/file.h"
 
 int stat(const char* pathname, struct stat* statbuf);
 int fstat(int fd, struct stat* statbuf);
