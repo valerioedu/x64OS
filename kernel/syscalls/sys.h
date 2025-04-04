4f6bd780ff0cf2e62456cb9d0ab4f8742387e8d5
Valerio <83879763+valerioedu@users.noreply.github.com>
2025-04-04 23:55:19 +0200

    I temporarily brought all syscalls into headers for makefile simplicity

--- kernel/syscalls/sys.h
+++ kernel/syscalls/sys.h
@@ -3,6 +3,10 @@
 
 #include "sys_read/read.h"
 #include "sys_write/write.h"
+#include "sys_open/open.h"
+#include "sys_close/close.h"
+#include "sys_stat/stat.h"
+#include "sys_sleep/sleep.h"
 
 typedef struct syscall_t {
     int syscall_no;
@@ -11,7 +15,12 @@ typedef struct syscall_t {
 
 syscall_t syscall_table[] = {
     {0, &read},
-    {1, &write}
+    {1, &write},
+    {2, &open},
+    {3, &close},
+    {4, &stat},
+    {5, &fstat},
+    {6, &sleep}
 };
 
 #endif
--- kernel/syscalls/sys_read/read.h
+++ kernel/syscalls/sys_read/read.h
@@ -2,6 +2,7 @@
 #define READ_H
 
 #include "../../../lib/definitions.h"
+#include "../io.h"
 
 ssize_t read(int fd, void* buf, size_t count);
 
